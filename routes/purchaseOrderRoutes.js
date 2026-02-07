546d8f00f6f9c663fef6324bb9c579f18cab5702
Roy Shuvo <roy247@auronibd.com>
2026-02-07 16:44:38 +0600

    ok

--- routes/purchaseOrderRoutes.js
+++ routes/purchaseOrderRoutes.js
@@ -5,6 +5,7 @@ const checkPermission = require('../middleware/permissionMiddleware'); // Added
 
 router.get('/', checkPermission('prod_po'), poController.getPOList);
 router.get('/search-product', checkPermission('prod_po'), poController.searchProductForPO);
+router.get('/api/search-materials', checkPermission('prod_po'), poController.getMaterialsForPO);
 router.get('/variant-snapshot/:id', checkPermission('prod_po'), poController.getProductVariantsSnapshot);
 router.post('/create', checkPermission('prod_po'), poController.createPurchaseOrder);
 router.post('/receive/:id', checkPermission('prod_po'), poController.receivePurchaseOrder);
